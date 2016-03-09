c4abd6f2a97068357007414c2300452d3a5a26a7
Alex Krzos <akrzos@redhat.com>
2016-03-09 10:36:00 -0500

    Handle ansible quote trolling.
    + Simplify by removing many quotes
    + Dictionary of images to download and export into glance
    + Address install from undercloud and browbeat directory exists already
    
    Change-Id: I7f61face267036c86359cea1675ea7a6ff6458b0

--- ansible/install/filter_plugins/browbeat_install_filters.py
+++ ansible/install/filter_plugins/browbeat_install_filters.py
@@ -1,7 +1,13 @@
+def dict_remove(the_dict, item):
+    """Remove an item from a dictionary."""
+    del the_dict[item]
+    return the_dict
+
+
 def hosts_to_dictionary(arg):
     """Changes list format of hosts to dictionary format.  The key of the dictionary is the index
     of the host. The index is defined by the host's suffix, example: overcloud-controller-10 is 10.
-    If there is no suffix, I use an increamented value above 1000000."""
+    If there is no suffix, I use an incremented value above 1000000."""
 
     dictionary = {}
     nonindex = 1000000
@@ -17,4 +23,7 @@ def hosts_to_dictionary(arg):
 
 class FilterModule(object):
     def filters(self):
-      return {'hosts_to_dictionary': hosts_to_dictionary}
+      return {
+        'dict_remove': dict_remove,
+        'hosts_to_dictionary': hosts_to_dictionary,
+        }
