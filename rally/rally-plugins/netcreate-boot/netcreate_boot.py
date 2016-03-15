5d84bdc3f2645424670c437b939868f84f91656a
Joe <joe.talerico@gmail.com>
2016-03-15 16:55:06 -0400

    Add Rally Plugins
    Add Rally Plugin to our automation.
    
    + Restructure directory
    + Patch some of the code
    + Patched up lib/Rally, still need to finish a couple of changes
    + Added Plugin path to config
    + Remove debugging
    + Update the yml files for plugins
    
    Change-Id: I6059ef55ecce1bd32301bdf8ad386d993bbdf757

--- rally/rally-plugins/netcreate-boot/netcreate_boot.py
+++ rally/rally-plugins/netcreate-boot/netcreate_boot.py
@@ -9,26 +9,18 @@ from rally.task import validation
 class NeutronPlugin(neutron_utils.NeutronScenario,
                     nova_utils.NovaScenario,
                     scenario.Scenario):
-
-    #
     # Create network
     # Create subnet
-    # Attach to router
     # Attach guest to new network
-    # List
-    # Ping
     # Cleanup
-    #
     @types.set(image=types.ImageResourceType,
                flavor=types.FlavorResourceType)
     @validation.image_valid_on_flavor("flavor", "image")
     @validation.required_openstack(users=True)
     @scenario.configure(context={"cleanup": ["nova","neutron"]})
-    def create_network_nova_boot(self,image,flavor,router,
-                                 network_create_args=None,subnet_create_args=None,
-                                 **kwargs):
+    def create_network_nova_boot(self,image,flavor,network_create_args=None,
+                                 subnet_create_args=None,**kwargs):
         network = self._create_network(network_create_args or {})
         subnet = self._create_subnet(network, subnet_create_args or {})
-        self._add_interface_router(subnet['subnet'],router)
         kwargs["nics"] = [{ 'net-id': network['network']['id']}]
         self._boot_server(image, flavor, **kwargs)
