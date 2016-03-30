13c67b4e1108a71ca9f52fdee99b0b3af2233cd3
Joe Talerico <jtaleric@redhat.com>
2016-03-30 17:50:18 -0400

    Elastic search connector - Rally
    + Added function for Rally Results
    + Modified the Service filed
    + Rally.py now produces a json file and returns json
    + Grafana to return the URL list
    + Quick single workload test worked.. +1
    + Switched to a List for the config
    + Rebase
    + Remove parent/child work
    + Error check
    + Default 3 jsons in the config file
    + Rebase
    + Pep
    + Remove Grafana-API Key per Akrzos
    + Small bug
    + Removed getter/setter in Grafana.py
    
    Change-Id: I9f1d5f27597c4c4897fc4ee821aad309fa11a04b

---
+++ lib/Elastic.py
@@ -0,0 +1,63 @@
+from elasticsearch import Elasticsearch
+import logging
+import json
+import pprint
+import numpy
+
+class Elastic:
+    """
+    """
+    def __init__(self,config,tool="browbeat") :
+        self.config = config
+        self.logger = logging.getLogger('browbeat.Elastic')
+        self.es = Elasticsearch([
+            {'host': self.config['elasticsearch']['host'],
+             'port': self.config['elasticsearch']['port']}],
+            send_get_body_as='POST'
+        )
+        self.index = tool
+
+    """
+    """
+    def load_json(self,result):
+        json_data = None
+        self.logger.info("Loading JSON")
+        json_data = json.loads(result)
+        return json_data
+
+    """
+    """
+    def load_json_file(self,result):
+        json_data = None
+        self.logger.info("Loading JSON file : {}".format(result))
+        try :
+            with open(result) as jdata :
+                json_data = json.load(jdata)
+        except (IOError, OSError) as e:
+            self.logger.error("Error loading JSON file : {}".format(result))
+            return False
+        return json_data
+
+    """
+    """
+    def combine_metadata(self,result):
+        if len(self.config['elasticsearch']['metadata_files']) > 0 :
+            meta = self.config['elasticsearch']['metadata_files']
+            for _meta in meta:
+                try :
+                    with open(_meta['file']) as jdata :
+                        result[_meta['name']] = json.load(jdata)
+                except (IOError, OSError) as e:
+                    self.logger.error("Error loading Metadata file : {}".format(_meta['file']))
+                    return False
+            return result
+
+    """
+    """
+    def index_result(self,result,_id=None) :
+        return self.es.index(index=self.index,
+                             id=_id,
+                             body=result,
+                             doc_type='result',
+                             refresh=True
+                             )
--- lib/Grafana.py
+++ lib/Grafana.py
@@ -12,12 +12,11 @@ class Grafana:
         self.grafana_ip = self.config['grafana']['grafana_ip']
         self.grafana_port = self.config['grafana']['grafana_port']
         self.playbook = self.config['ansible']['grafana_snapshot']
+        self.grafana_url = []
 
-    def get_extra_vars(self, from_ts, to_ts, result_dir, test_name):
-        extra_vars = 'grafana_ip={} '.format(
-            self.config['grafana']['grafana_ip'])
-        extra_vars += 'grafana_port={} '.format(
-            self.config['grafana']['grafana_port'])
+    def extra_vars(self, from_ts, to_ts, result_dir, test_name):
+        extra_vars = 'grafana_ip={} '.format(self.config['grafana']['grafana_ip'])
+        extra_vars += 'grafana_port={} '.format(self.config['grafana']['grafana_port'])
         extra_vars += 'from={} '.format(from_ts)
         extra_vars += 'to={} '.format(to_ts)
         extra_vars += 'results_dir={}/{} '.format(result_dir, test_name)
@@ -26,19 +25,26 @@ class Grafana:
             extra_vars += 'snapshot_compute=true '
         return extra_vars
 
-    def print_dashboard_url(self, from_ts, to_ts, test_name):
+    def grafana_urls(self):
+        return self.grafana_url
+
+    def create_grafana_urls(self, time):
         if 'grafana' in self.config and self.config['grafana']['enabled']:
+            from_ts = time['from_ts']
+            to_ts = time['to_ts']
             url = 'http://{}:{}/dashboard/db/'.format(
                 self.grafana_ip, self.grafana_port)
             for dashboard in self.config['grafana']['dashboards']:
-                full_url = '{}{}?from={}&to={}&var-Cloud={}'.format(
-                    url, dashboard, from_ts, to_ts, self.cloud_name)
-                self.logger.info(
-                    '{} - Grafana URL: {}'.format(test_name, full_url))
+                self.grafana_url.append('{}{}?from={}&to={}&var-Cloud={}'.format(
+                    url, dashboard, from_ts, to_ts, self.cloud_name))
+
+    def print_dashboard_url(self,test_name):
+        for full_url in self.grafana_url:
+            self.logger.info('{} - Grafana URL: {}'.format(test_name, full_url))
 
     def log_snapshot_playbook_cmd(self, from_ts, to_ts, result_dir, test_name):
         if 'grafana' in self.config and self.config['grafana']['enabled']:
-            extra_vars = self.get_extra_vars(
+            extra_vars = self.extra_vars(
                 from_ts, to_ts, result_dir, test_name)
             snapshot_cmd = 'ansible-playbook -i {} {} -e "{}"'.format(
                 self.hosts_file, self.playbook, extra_vars)
@@ -47,12 +53,10 @@ class Grafana:
     def run_playbook(self, from_ts, to_ts, result_dir, test_name):
         if 'grafana' in self.config and self.config['grafana']['enabled']:
             if self.config['grafana']['snapshot']['enabled']:
-                extra_vars = self.get_extra_vars(
+                extra_vars = self.extra_vars(
                     from_ts, to_ts, result_dir, test_name)
                 subprocess_cmd = ['ansible-playbook', '-i', self.hosts_file, self.playbook, '-e',
                                   '{}'.format(extra_vars)]
                 snapshot_log = open('{}/snapshot.log'.format(result_dir), 'a+')
-                self.logger.info(
-                    'Running ansible to create snapshots for: {}'.format(test_name))
-                subprocess.Popen(
-                    subprocess_cmd, stdout=snapshot_log, stderr=subprocess.STDOUT)
+                self.logger.info('Running ansible to create snapshots for: {}'.format(test_name))
+                subprocess.Popen(subprocess_cmd, stdout=snapshot_log, stderr=subprocess.STDOUT)
