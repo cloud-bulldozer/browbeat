39cf6ce92e313e3c5ca9abb1c5b52200fa09d210
akrzos <akrzos@redhat.com>
2016-12-14 21:09:46 -0500

    Collectd plugin: Gnocchi status
    Initial commit to add a plugin to collectd for obtaining and storing
    Gnocchi status metrics.  This plug obtains the two metrics provided
    by Gnocchi's REST API for "the number of metrics having measures to be
    processed" and "the number of measures to process".
    
    This also addes an associated graph in the General OpenStack System
    Performance Dashboard and the Controller specific dashboard.
    
    * no longer requires authentication details in the collectd config
    * removing the redis exec plugin
    * configurable interval for the python plugin now
    
    Change-Id: Idfc5c719e9b0fbaad5db0632a3e5293bf03d7ab6

--- ansible/install/roles/collectd-openstack/files/collectd-redis.sh
+++
@@ -1,29 +0,0 @@
-#!/bin/bash
-
-HOSTNAME="${COLLECTD_HOSTNAME:-`hostname -f`}"
-INTERVAL="${COLLECTD_INTERVAL:-10}"
-PORT=6379
-
-while true
-do
-
-info=$((echo info ; sleep 2) |nc -w 1 $HOSTNAME $PORT 2>&1)
-connected_clients=$(echo "$info" | egrep ^connected_clients| awk -F: '{ print $2 }' | sed 's///g')
-connected_slaves=$(echo "$info" | egrep ^connected_slaves| awk -F: '{ print $2 }' | sed 's///g')
-uptime=$(echo "$info" | egrep ^uptime_in_seconds| awk -F: '{ print $2 }' | sed 's///g')
-used_memory=$(echo "$info" | egrep ^used_memory:| awk -F: '{ print $2 }' | sed 's///g')
-changes_since_last_save=$(echo "$info" | egrep ^rdb_changes_since_last_save| awk -F: '{ print $2 }' | sed 's///g')
-total_commands_processed=$(echo "$info" | egrep ^total_commands_processed| awk -F: '{ print $2 }' | sed 's///g')
-keys=$(echo "$info" | egrep ^db0:keys| awk -F= '{ print $2 }' | awk -F, '{ print $1 }' | sed 's///g')
-
-echo "PUTVAL $HOSTNAME/redis-$PORT/memcached_connections-clients interval=$INTERVAL N:$connected_clients"
-echo "PUTVAL $HOSTNAME/redis-$PORT/memcached_connections-slaves interval=$INTERVAL N:$connected_slaves"
-echo "PUTVAL $HOSTNAME/redis-$PORT/uptime interval=$INTERVAL N:$uptime"
-echo "PUTVAL $HOSTNAME/redis-$PORT/df-memory interval=$INTERVAL N:$used_memory:U"
-echo "PUTVAL $HOSTNAME/redis-$PORT/files-unsaved_changes interval=$INTERVAL N:$changes_since_last_save"
-echo "PUTVAL $HOSTNAME/redis-$PORT/memcached_command-total interval=$INTERVAL N:$total_commands_processed"
-echo "PUTVAL $HOSTNAME/redis-$PORT/memcached_items-db0 interval=$INTERVAL N:$keys"
-
-sleep "$INTERVAL"
-done
-
