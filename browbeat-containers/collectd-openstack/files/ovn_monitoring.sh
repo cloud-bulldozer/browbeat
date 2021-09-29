1ea5db8de143eb42e4095b2289670bea43e60d98
Sanjay Chari <schari@redhat.com>
2021-09-29 18:50:40 +0530

    More changes in OVN monitoring
    This patch introduces the following changes.
    1. The ovn_monitoring.sh script has been changed to accurately depict
    the row count in each table.
    2. Adding stack user to wheel group has been moved to after the container
    has been built and is running.
    
    Change-Id: I2e7fed2fd3245832689521cd5d7f544f5a476254

--- browbeat-containers/collectd-openstack/files/ovn_monitoring.sh
+++ browbeat-containers/collectd-openstack/files/ovn_monitoring.sh
@@ -4,5 +4,6 @@ INTERVAL="${COLLECTD_INTERVAL:-15}"
 
 while sleep "$INTERVAL"; do
   VALUE=$(sudo ovsdb-client dump --no-headings unix:/var/lib/openvswitch/ovn/ovn$1_db.sock $2 | wc -l)
+  VALUE=$[VALUE-1]
   echo "PUTVAL \"$HOSTNAME/ovn-$1db-$2/gauge-ovn_$1db_$2\" interval=$INTERVAL N:$VALUE"
 done
