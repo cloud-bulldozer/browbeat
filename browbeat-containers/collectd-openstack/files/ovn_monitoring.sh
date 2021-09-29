4a7c38b6bc170827f822f520c24c1289fc55125a
Sanjay Chari <schari@redhat.com>
2021-09-29 13:34:14 +0530

    Change in OVN monitoring
    This patch changes the OVN monitoring collectd plugin and Grafana
    panels to include database name(nbdb or sbdb) along with the table
    name for the metrics.
    
    Change-Id: I9c8c034a591242d6d6c11f9cac4caed4fec20971

--- browbeat-containers/collectd-openstack/files/ovn_monitoring.sh
+++ browbeat-containers/collectd-openstack/files/ovn_monitoring.sh
@@ -3,6 +3,6 @@ HOSTNAME="${COLLECTD_HOSTNAME:-`hostname -f`}"
 INTERVAL="${COLLECTD_INTERVAL:-15}"
 
 while sleep "$INTERVAL"; do
-  VALUE=$(sudo ovsdb-client dump unix:/var/lib/openvswitch/ovn/ovn$1_db.sock $2 | wc -l)
-  echo "PUTVAL \"$HOSTNAME/ovn-$2/gauge-ovn_$2\" interval=$INTERVAL N:$VALUE"
+  VALUE=$(sudo ovsdb-client dump --no-headings unix:/var/lib/openvswitch/ovn/ovn$1_db.sock $2 | wc -l)
+  echo "PUTVAL \"$HOSTNAME/ovn-$1db-$2/gauge-ovn_$1db_$2\" interval=$INTERVAL N:$VALUE"
 done
