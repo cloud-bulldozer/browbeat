9b5c8101fbb44751c338a4004f0727b17c10296d
Masco <mkaliyam@redhat.com>
2024-07-16 17:28:32 +0530

    add tail plugin support for rhoso
    Change-Id: Idb982e309610d5c9a452ab5b73a481ef81974e0f

--- ansible/install/roles/collectd-rhoso/files/entrypoint.sh
+++ ansible/install/roles/collectd-rhoso/files/entrypoint.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-# Get the last segment of the pod name (assuming the pod name is stored in POD_NAME environment variable)
-POD_NAME_SUFFIX=$(echo $POD_NAME | awk -F '-' '{print $NF}')
-
-# Determine the configuration file based on the pod name suffix
-if [ "$POD_NAME_SUFFIX" = "0" ]; then
-  CONFIG_FILE="/etc/config/collectd-db.conf"
-else
-  CONFIG_FILE="/etc/config/collectd.conf"
-fi
-
+# Determine the configuration file based on the node name
+CONFIG_FILE="/etc/config/${NODE_NAME}.conf"
 # Start the main process with the selected configuration file
 exec collectd -f -C $CONFIG_FILE
