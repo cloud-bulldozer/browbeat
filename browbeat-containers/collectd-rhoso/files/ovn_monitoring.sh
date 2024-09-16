3bf8661826e20d3e69c1c47bf24ea84874f03e26
rajeshP524 <rajeshpulapakula24@gmail.com>
2024-09-16 16:31:59 +0530

    Add support for OVN monitoring when TLS enabled
    Added OVNSB and OVNNB cert secrets to ospperf namespace and mounted
    them on to collectd container. Ovsdb-client uses these mounted ssl
    certificates to connect with ovsdb-server from within collectd pod.
    
    Change-Id: Id89b495a73350b7cb25ce23f069a49d3a9ff49d9

--- browbeat-containers/collectd-rhoso/files/ovn_monitoring.sh
+++ browbeat-containers/collectd-rhoso/files/ovn_monitoring.sh
@@ -5,13 +5,23 @@ INTERVAL="${COLLECTD_INTERVAL:-15}"
 if [ "$1" = "sb" ]; then
   IP=$OVN_SBDB_IP
   PORT=$OVN_SBDB_PORT
+  DB="ovnsb"
 else
   IP=$OVN_NBDB_IP
   PORT=$OVN_NBDB_PORT
+  DB="ovnnb"
 fi
 
+PRIVATE_KEY="/etc/pki/$DB/tls/private/ovndb.key"
+CERTIFICATE="/etc/pki/$DB/tls/certs/ovndb.crt"
+CA_CERT="/etc/pki/$DB/tls/certs/ovndbca.crt"
+
 while sleep "$INTERVAL"; do
-  VALUE=$(sudo ovsdb-client dump --no-headings tcp:$IP:$PORT $2 | wc -l)
+  VALUE=$(sudo ovsdb-client dump --no-headings ssl:$IP:$PORT \
+    --private-key=$PRIVATE_KEY \
+    --certificate=$CERTIFICATE \
+    --ca-cert=$CA_CERT \
+    $2 | wc -l)
   VALUE=$[VALUE-1]
   echo "PUTVAL \"$HOSTNAME/ovn-$1db-$2/gauge-ovn_$1db_$2\" interval=$INTERVAL N:$VALUE"
 done
